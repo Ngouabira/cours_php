ec8ea4d39e3fbc7eb7353f4cda5e83aa60b6f5eb
Valdy NGOUABIRA <valdyngouabira@gmail.com>
2025-03-19 08:29:51 +0000

    Modification fichier variable.

--- variable.php
+++ variable.php
@@ -1,20 +1,26 @@
 <?php
 
+//Une variable est caractérisée par son type, son nom et sa valeur
+
+//une chaîne de caractère
 $name = "John";
 
 echo $name . "<br>";
 echo gettype($name) . "<br>";
 
+//Un entier
 $age = 24;
 
 echo $age . "<br>";
 echo gettype($age) . "<br>";
 
+//Booléen
 $test = true;
 
 echo $test . "<br>";
 echo gettype($test) . "<br>";
 
+//Nombre décimal
 $number = 12.5;
 
 echo $number . "<br>";
